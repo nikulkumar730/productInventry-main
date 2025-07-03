2eceec46276399bc07cd5e9ab967b2bad384589f
nikulkumar730 <nikulkumar730@gmail.com>
2025-07-03 11:37:42 +0530

    added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const userRegister= require('./routes/userRoutes')
 const product=require('./routes/productRoutes')
 const cors = require('cors');
 require('dotenv').config();
+app.use(cors({
+    origin: 'http://localhost:5173', // Update this if your Vite dev server runs on a different port
+    credentials: true
+}));
 app.use(express.json());
 app.use('/api/user',userRegister);
 app.use('/api/product',product);
@@ -13,4 +17,3 @@ const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}...`);
 });
-app.use(cors());
